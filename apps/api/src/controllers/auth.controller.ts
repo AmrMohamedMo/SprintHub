15682266f6c7dc28a8517864841a01844a19f1c0
amr <amr@gmail.com>
2026-07-26 14:25:42 +0300

    refactor: improve auth module structure

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -12,3 +12,10 @@ export const login = async (req: Request, res: Response) => {
   const result = await loginUser(req.body);
   res.status(200).json(result)
 }
+
+
+export const me = (req: Request, res: Response) => {
+  res.json({
+    user: req.user
+  })
+};
--- apps/api/src/types/express.d.ts
+++ apps/api/src/types/express.d.ts
@@ -1,9 +1,13 @@
-import "express";
-declare module "express-serve-static-core" {
-  interface Request {
-    user: {
-      userId: string,
-      email:string
+export { };
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        userId: string;
+        email: string;
+        role: string;
+      };
     }
   }
 }
